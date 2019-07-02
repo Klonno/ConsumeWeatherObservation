2cc17dde2aaa58cc310680245a95026b0af2dc67
Scott Teerink <Scott.Teerink@PREMERA.com>
2019-07-02 14:09:30 -0700

    Add project files.

---
+++ ConsumeWeatherObservation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsumeWeatherObservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
